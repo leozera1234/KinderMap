410907fa88a57e5042664794e685cb7bd3dba88f
CamilaFernandesdev <camila.fernandes.fayer@gmail.com>
2026-03-01 03:35:03 -0300

    [refactor] Replace KinderMapRootView with WelcomeView and remove unused ChildProfile and Gender models

--- Model/Profile/Gender.swift
+++ Model/Profile/Gender.swift
@@ -1,9 +1,9 @@
 import Foundation
 
 enum Gender: String, CaseIterable, Identifiable {
-    case female = "Feminino"
-    case male = "Masculino"
-    case unspecified = "Prefiro não informar"
+    case female = "Female"
+    case male = "Male"
+    case unspecified = "I don't want to inform"
 
     var id: String { rawValue }
 }
